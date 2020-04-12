3048da5594b4e2bdf58503412121a124c1957efb
Victor Marques <victormatheus92@gmail.com>
2020-04-12 13:58:57 -0300

    criação do componente Input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { TiInfoLarge } from 'react-icons/ti'
+import InputMask from 'react-input-mask'
+
+// Styles
+import { BoxInput } from './styles.js'
+import { Colors } from '../../styles/styleds.js'
+
+export default class Input extends Component {
+	state = {
+		type: this.props.type,
+		errorMessage: ''
+	}
+
+	render() {
+		return (
+			<BoxInput mgBottom={this.props.mgBottom}>
+				<InputMask id={this.props.id}
+					type={this.state.type || 'text'}
+					ref={input => { this.textInput = input }}
+					value={this.props.value}
+					onChange={this.props.onChange}
+					onBlur={() => this.checkValidate()}
+					onKeyPress={this.props.onKeyPress}
+					mask={this.props.mask}
+					maskChar=""
+					maxLength={this.props.maxLength}
+					style={{ borderBottomColor: this.state.errorMessage ? Colors.danger : Colors.silver }}
+					required
+				/>
+				<label htmlFor={this.props.id}>
+					{this.props.label}
+
+					{
+						this.props.info &&
+						<div className="info">
+							<TiInfoLarge />
+						</div>
+					}
+				</label>
+
+				{
+					this.state.errorMessage &&
+					<p className="error">{this.state.errorMessage}</p>
+				}
+			</BoxInput>
+		)
+	}
+
+	// ============================================
+	//                                    Functions
+	// ============================================
+	checkValidate() {
+		if (this.props.validate) {
+			this.setState({ errorMessage: this.props.validate() ? '' : this.props.errorMessage })
+		}
+	}
+}
---
+++ src/components/Input/styles.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components'
+import { Colors } from '../../styles/styleds'
+
+export const BoxInput = styled.div `
+	height: 65px;
+	margin-bottom: ${props => props.mgBottom ? props.mgBottom : 0}px;
+	position: relative;
+
+	label {
+		align-items: center;
+		color: ${Colors.silver};
+		display: flex;
+		font-size: 17px;
+		line-height: 22px;
+		pointer-events: none;
+		position: absolute;
+		top: 25px;
+		transition: .25s ease-in-out;
+
+		.info {
+			align-items: center;
+			background-color: ${Colors.silver};
+			border-radius: 50%;
+			color: ${Colors.white};
+			display: flex;
+			font-size: 9px;
+			height: 13px;
+			justify-content: center;
+			margin-left: 10px;
+			width: 13px;
+		}
+	}
+
+	input {
+		background-color: transparent;
+		border-bottom: 1px solid;
+		color: ${Colors.grey};
+		display: block;
+		font-size: 17px;
+		line-height: 22px;
+		padding: 20px 0 5px;
+		width: 100%;
+
+		&:focus {
+			&~label {
+				font-size: 13px;
+				top: 0;
+			}
+		}
+
+		&:valid {
+			&~label {
+				font-size: 13px;
+				top: 0;
+			}
+		}
+	}
+
+	button {
+		background: transparent;
+		cursor: pointer;
+		height: 20px;
+		opacity: .4;
+		position: absolute;
+		right: 4px;
+		top: 26px;
+		width: 20px;
+	}
+
+	.error {
+		color: ${Colors.danger};
+		font-size: 12px;
+		margin-top: 5px;
+	}
+`
